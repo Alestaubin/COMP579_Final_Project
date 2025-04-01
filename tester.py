1582841f2537c44d4e3fd13eae103c77276a38cb
Alestaubin <alexandre.st-aubin2@mail.mcgill.ca>
2025-04-01 10:41:48 -0400

    tester code

--- tester.py
+++ tester.py
@@ -1,6 +1,7 @@
 import gym
 import abides_gym
 
+print("Initializing rmsc04 gym environment...")
 env = gym.make(
     "markets-daily_investor-v0",
     background_config="rmsc04",
@@ -8,5 +9,7 @@ env = gym.make(
 
 env.seed(0)
 initial_state = env.reset()
+print("Trying to run 5 steps in the environment:")
 for i in range(5):
     state, reward, done, info = env.step(0)
+    print("state: ", state, "\nreward: ", reward)
