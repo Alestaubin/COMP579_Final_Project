ba31f2b53b500751f748673b14866dc16cdec599
Alestaubin <alexandre.st-aubin2@mail.mcgill.ca>
2025-04-17 16:03:35 -0400

    skrl model

--- src/oterl/__main__.py
+++ src/oterl/__main__.py
@@ -1,6 +1,10 @@
 import abides_gym
 import gym
-
+from oterl.utils.cfg_utils import get_ppo_cartpole_cfg
+from oterl.models import Policy, Value
+from oterl.training.trainer import train_agent
+from skrl.agents.torch.ppo import PPO
+from skrl.envs.wrappers.torch import wrap_env
 
 def main():
   print('Initializing rmsc04 gym environment...')
@@ -9,13 +13,12 @@ def main():
     'markets-daily_investor-v0',
     background_config='rmsc04',
   )
+  env = wrap_env(env)
+  seed = 0 
 
   env.reset()
+  env.seed(seed)
 
-  env.seed(0)
-
-  print('Trying to run 5 steps in the environment:')
-
-  for _ in range(5):
-    state, reward, _, _ = env.step(0)
-    print('state: ', state, '\nreward: ', reward)
+  for timesteps in [500_000, 1_000_000, 2_000_000]:  # Different training durations
+    print(f"Training PPO with {timesteps} timesteps and seed {seed}")
+    train_agent(PPO, env, cfg=None, timesteps=timesteps, seed=seed)
---
+++ src/oterl/utils/cfg_utils.py
@@ -0,0 +1,31 @@
+from skrl.agents.torch.ppo import PPO, PPO_DEFAULT_CONFIG
+from skrl.resources.schedulers.torch import KLAdaptiveRL
+from skrl.resources.preprocessors.torch import RunningStandardScaler
+
+def get_ppo_cartpole_cfg(env, device):
+    cfg = PPO_DEFAULT_CONFIG.copy()
+    cfg["rollouts"] = 1024  # Memory size
+    cfg["learning_epochs"] = 10
+    cfg["mini_batches"] = 32
+    cfg["discount_factor"] = 0.99
+    cfg["lambda"] = 0.95
+    cfg["learning_rate"] = 1e-3
+    cfg["learning_rate_scheduler"] = KLAdaptiveRL
+    cfg["learning_rate_scheduler_kwargs"] = {"kl_threshold": 0.008}
+    cfg["grad_norm_clip"] = 0.5
+    cfg["ratio_clip"] = 0.2
+    cfg["value_clip"] = 0.2
+    cfg["clip_predicted_values"] = False
+    cfg["entropy_loss_scale"] = 0.01
+    cfg["value_loss_scale"] = 0.5
+    cfg["kl_threshold"] = 0
+    cfg["mixed_precision"] = True
+    cfg["state_preprocessor"] = RunningStandardScaler
+    cfg["state_preprocessor_kwargs"] = {"size": env.observation_space, "device": device}
+    cfg["value_preprocessor"] = RunningStandardScaler
+    cfg["value_preprocessor_kwargs"] = {"size": 1, "device": device}
+    # Logging to TensorBoard and checkpoints
+    cfg["experiment"]["write_interval"] = 500
+    cfg["experiment"]["checkpoint_interval"] = 5000
+    cfg["experiment"]["directory"] = "runs/torch/CartPole"
+    return cfg
