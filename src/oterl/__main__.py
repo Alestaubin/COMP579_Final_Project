70486c641532bfad5c525d3ebe532b468042de9d
Alestaubin <alexandre.st-aubin2@mail.mcgill.ca>
2025-04-19 19:29:43 -0400

    Added order book to the state

--- src/oterl/__main__.py
+++ src/oterl/__main__.py
@@ -5,6 +5,7 @@ from oterl.models import Policy, Value
 from oterl.training.trainer import train_agent
 from skrl.agents.torch.ppo import PPO
 from skrl.envs.wrappers.torch import wrap_env
+import pdb;pdb.set_trace()
 
 def main():
   print('Initializing rmsc04 gym environment...')
@@ -13,6 +14,17 @@ def main():
     'markets-daily_investor-v0',
     background_config='rmsc04',
   )
+  env.reset()
+  for i in range(10):
+    state, reward, done, info = env.step(0)
+    print("Orderbook:", info['orderbook'])
+    # Last 40 entries of the state are 10 levels of the orderbook in the form : 
+    # bid_prices (10), bid_volumes (10), ask_prices (10), ask_volumes (10)
+    # The first 7 entries of the state are the original 7 features
+    print("State:", state)
+
+
+  breakpoint()
   env = wrap_env(env)
   seed = 0 
 
@@ -21,4 +33,4 @@ def main():
 
   for timesteps in [500_000, 1_000_000, 2_000_000]:  # Different training durations
     print(f"Training PPO with {timesteps} timesteps and seed {seed}")
-    train_agent(PPO, env, cfg=None, timesteps=timesteps, seed=seed)
+    train_agent(PPO, env, cfg=None, timesteps=timesteps, seed=seed
