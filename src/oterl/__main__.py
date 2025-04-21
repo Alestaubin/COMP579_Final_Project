9011191d8f1e7f0aa3d19835849f765a08d6a879
Alestaubin <alexandre.st-aubin2@mail.mcgill.ca>
2025-04-21 14:12:11 -0400

    started implementing recurrrent PPO

--- src/oterl/__main__.py
+++ src/oterl/__main__.py
@@ -1,36 +1,83 @@
+import yaml
 import abides_gym
 import gym
 from oterl.utils.cfg_utils import get_ppo_cartpole_cfg
 from oterl.models import Policy, Value
-from oterl.training.trainer import train_agent
+from oterl.agents.baselines.trainer import train_agent
 from skrl.agents.torch.ppo import PPO
+from skrl.agents.torch.dqn import DQN, DQN_DEFAULT_CONFIG
+
 from skrl.envs.wrappers.torch import wrap_env
 import pdb;pdb.set_trace()
+from oterl.agents.recurrent_ppo.trainer import Trainer
+
+def load_config(config_path):
+    with open(config_path, 'r') as f:
+        return yaml.safe_load(f)
 
 def main():
   print('Initializing rmsc04 gym environment...')
+  config_path = '/Users/alexst-aubin/Library/CloudStorage/GoogleDrive-alex.staubin2@gmail.com/My Drive/McGill/McGill_Winter25/COMP579/FinalProject/code/src/oterl/config.yaml'
+  cfg = load_config(config_path)
 
   env = gym.make(
     'markets-daily_investor-v0',
     background_config='rmsc04',
   )
-  env.reset()
-  for i in range(10):
-    state, reward, done, info = env.step(0)
-    print("Orderbook:", info['orderbook'])
-    # Last 40 entries of the state are 10 levels of the orderbook in the form : 
-    # bid_prices (10), bid_volumes (10), ask_prices (10), ask_volumes (10)
-    # The first 7 entries of the state are the original 7 features
-    print("State:", state)
+  seed = cfg.get("seed", 0)
 
+  for timesteps in cfg["timesteps"]:
+      for agent in cfg["agents"]:
+          print(f"Training {agent} with {timesteps} timesteps and seed {cfg.get('seed', 0)}")
+          if agent == "PPO":
+            # The SKRL library requires the environment to be wrapped in a specific way
+            env = wrap_env(env)
+            env.reset()
+            env.seed(seed)
+            train_agent(PPO, env, cfg=None, timesteps=timesteps, seed=cfg.get("seed", 0))
+          elif agent == "DQN":
+            # The SKRL library requires the environment to be wrapped in a specific way
+            env = wrap_env(env)
+            env.reset()
+            env.seed(seed)
+            train_agent(DQN, env, cfg=None, timesteps=timesteps, seed=cfg.get("seed", 0))
+          elif agent == "RPPO":
+            max_eps_length = 1000
 
-  breakpoint()
-  env = wrap_env(env)
-  seed = 0 
+            config = {
+                "PPO":{
+                    "critic_coef": 1,
+                    "policy_kl_range":0.0008,
+                    "policy_params": 20,
+                    "gamma":0.998,
+                    "gae_lambda":0.95,
+                    "value_clip": 0.2,
 
-  env.reset()
-  env.seed(seed)
-
-  for timesteps in [500_000, 1_000_000, 2_000_000]:  # Different training durations
-    print(f"Training PPO with {timesteps} timesteps and seed {seed}")
-    train_agent(PPO, env, cfg=None, timesteps=timesteps, seed=seed
+                },
+                "LSTM":{
+                    "max_eps_length":max_eps_length + 50,
+                    "seq_length":-1,
+                    "hidden_size":64,
+                    "embed_size": 64,
+                },
+                "entropy_coef":{
+                    "start": 0.01,
+                    "end": 0,
+                    "step": 100_000
+                },
+                "lr":1e-3,
+                "num_epochs": 3,
+                "num_game_per_batch":1,
+                "max_grad_norm": 0.5,
+                "n_mini_batch": 4,
+                "rewards": [0,1,0], # [lose,win,not_done]
+                "set_detect_anomaly": True,
+                "normalize_advantage": True,
+            }
+            writer_path = "runs"
+            save_path = "models"
+            trainer = Trainer(config=config,env=env,writer_path = writer_path,save_path=save_path)
+            trainer.train()
+          else:
+            print(f"Unknown agent: {agent}")
+            break
---
+++ src/oterl/agents/recurrent_ppo/agent.py
@@ -0,0 +1,139 @@
+import torch
+import torch.nn.functional as F
+import numpy as np
+from oterl.agents.recurrent_ppo.rolloutBuffer import RolloutBuffer
+from oterl.agents.recurrent_ppo.distribution import Distribution
+from oterl.agents.recurrent_ppo.stat import RunningMeanStd
+from tqdm import tqdm
+
+class Agent():
+    """Agent class representing an AI agent"""
+    def __init__(self, env, model, config):
+        """
+        Overview:
+            Initializes the Agent instance.
+
+        Arguments:
+            - env: (`object`): The environment object.
+            - model: (`object`): The model object.
+            - config: (`dict`): Configuration settings for the agent.
+        """
+        super().__init__()
+        self.env            = env
+        self.observation_space_size = env.observation_space.shape[0]
+        self.action_space_size = env.action_space.n
+        
+        self.model          = model
+        self.reward         = config["rewards"]
+        self.max_eps_length = config["LSTM"]["max_eps_length"]
+        self.p_state        = torch.zeros(1, 1, config["LSTM"]["hidden_size"])
+        self.v_state        = torch.zeros(1, 1, config["LSTM"]["hidden_size"])
+        self.hidden_size    = config["LSTM"]["hidden_size"]
+        self.rollout        = RolloutBuffer(config, self.observation_space_size, self.action_space_size)
+        self.distribution   = Distribution()
+        self.rms            = RunningMeanStd(shape=(self.observation_space_size))
+
+    def reset_hidden(self):
+        """
+        Overview:
+            Resets the hidden state and candidate state.
+        """
+        self.p_state = torch.zeros(1, 1, self.hidden_size)
+        self.v_state = torch.zeros(1, 1, self.hidden_size)
+
+    @torch.no_grad()
+    def play(self, state):
+        """
+        Overview:
+            Agent's play function.
+
+        Arguments:
+            - state: (`np.array`): The current state.
+            - per: (`List`): The per file.
+
+        Returns:
+            - action: (`int`): The agent's chosen action.
+            - per: (`List`): The per file.
+        """
+        self.model.eval()
+        tensor_state        = self.rms.normalize((torch.tensor(state, dtype=torch.float32)))
+        # print("tensor_state:", tensor_state)
+        policy, value, p_state, v_state = self.model(tensor_state.reshape(1, -1), self.p_state, self.v_state)
+        tensor_state        = tensor_state.view(-1)
+        policy              = policy.squeeze()
+        list_action         = np.arange(self.env.action_space.n)
+
+        action_mask         = torch.tensor(list_action, dtype=torch.float32)
+        action, log_prob    = self.distribution.sample_action(policy, action_mask)
+        
+        if action_mask[action] != 1:
+            action = np.random.choice(np.where(list_action == 1)[0])
+        
+        obs, reward, done, info = self.env.step(action)
+
+        if not done:  # game not over
+            if self.rollout.step_count < self.max_eps_length:
+                self.rollout.add_data(
+                    state        = tensor_state,
+                    p_state      = self.p_state.squeeze(),
+                    v_state      = self.v_state.squeeze(),
+                    action       = action,
+                    value        = value.item(),
+                    reward       = reward * 1.0,  # use the raw reward from step()
+                    done         = 0,
+                    valid_action = torch.from_numpy(list_action),
+                    prob         = log_prob,
+                    policy       = policy
+                )
+            self.rollout.step_count += 1
+        else:  # game over
+            if self.rollout.step_count < self.max_eps_length:
+                self.rollout.add_data(
+                    state        = tensor_state,
+                    p_state      = self.p_state.squeeze(),
+                    v_state      = self.v_state.squeeze(),
+                    action       = action,
+                    value        = value.item(),
+                    reward       = reward * 1.0,
+                    done         = 1,
+                    valid_action = torch.from_numpy(list_action),
+                    prob         = log_prob,
+                    policy       = policy
+                )
+
+            self.rollout.batch["dones_indices"][self.rollout.game_count] = self.rollout.step_count
+            self.rollout.game_count += 1
+            self.rollout.step_count = 0
+
+        self.p_state, self.v_state = p_state, v_state
+
+        if done:
+            self.reset_hidden()
+        
+        return obs, reward, done, info 
+    
+    def run(self, num_games: int) -> float:
+        """
+        Overview:
+            Runs the custom environment and returns the win rate.
+
+        Arguments:
+            - num_games: (`int`): The number of games to run.
+
+        Returns:
+            - win_rate: (`float`): The win rate of the agent.
+        """
+        # win_rate = self.env.run(self.play, num_games, np.array([0.]), 1)[0] / num_games
+        # self.rms.update()
+        # return win_rate
+        for i in tqdm(range(num_games), desc="Simulating games"):
+            # breakpoint()
+            obs = self.env.reset()
+            done = False
+            while not done:
+                obs, reward, done, info= self.play(obs)
+
+        self.rms.update()
+        
+        win_rate = 1
+        return win_rate
